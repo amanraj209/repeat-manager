d6e8346f1fa4a201a4b425d15dde5493294f525c
amanraj209 <amanraj20996@gmail.com>
2017-10-11 23:03:55 +0530

    Rename multiple files

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const os = require('os');
 
 const HOME_DIR = os.homedir();
-const MAIN_DIR = path.join(HOME_DIR, 'PATH_TO_FOLDER');
+const MAIN_DIR = path.join(HOME_DIR, 'Desktop', 'repeat-manager', 'Mac');
 
 function deleteFiles(DIR_PATH) {
     const files = fs.readdirSync(DIR_PATH);
@@ -32,4 +32,24 @@ function deleteFiles(DIR_PATH) {
     });
 }
 
-deleteFiles(MAIN_DIR);
+// deleteFiles(MAIN_DIR);
+
+function renameFiles(DIR_PATH) {
+    const files = fs.readdirSync(DIR_PATH);
+
+    files.sort();
+
+    let i = 1;
+    files.forEach(file => {
+        const ext = file.substr(file.lastIndexOf('.'));
+
+        const oldpath = path.join(DIR_PATH, file);
+        const newpath = path.join(DIR_PATH, String(i++) + ext);
+
+        fs.rename(oldpath, newpath, (err) => {
+            console.log(`${oldpath} renamed to ${newpath}`)
+        });
+    });
+}
+
+renameFiles(MAIN_DIR);
